1433407e3eb1e5388f19545405c00c6150587301
nmost <nick@mostowich.com>
2014-04-06 07:50:30 -0400

    change end condition to a sad ford

--- public/js/game.js
+++ public/js/game.js
@@ -54,39 +54,17 @@ var initializeGame = function(){
 
 
   Q.scene('endGame',function(stage) {
-    var container = stage.insert(new Q.UI.Container({
-      x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
-    }));
-
-    var button = container.insert(new Q.UI.Button({ 
-      x: 0, 
-      y: 0, 
-      fill: "#CCCCCC",
-      label: "Play Again" 
-    }));
-    var label = container.insert(new Q.UI.Text({
-      x:10, 
-        y: -10 - button.p.h, 
-      label: stage.options.label 
-    }));
-    button.on("click",function() {
-      Q.clearStages();
-      Q.stageScene('level1');
-    });
-
-    container.fit(20);
+    document.getElementById("fordimage").style.display="block";
   });
 
   Q.scene("gameStats", function(stage) {
     var statsContainer = stage.insert(new Q.UI.Container({
-        fill: "gray",
+        fill: "black",
        /* x: 960/2,
         y: 620,*/
         x:Q.width/2,
         y:20,
         border: 1,
-        shadow: 3,
-        shadowColor: "rgba(0,0,0,0.5)",
         w: 960,
         h: 40
       })
